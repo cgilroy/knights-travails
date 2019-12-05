36060cd365dd62490f2b1a6a583ef275aad868f6
CJ Gilroy <c.gilroy9@gmail.com>
2019-12-05 08:52:59 -0700

    Add method to find new move positions

--- KnightPathFinder.rb
+++ KnightPathFinder.rb
@@ -3,6 +3,7 @@ require 'byebug'
 class KnightPathFinder
     def initialize(pos)
         @root_node = PolyTreeNode.new(pos)
+        @considered_positions = [pos]
     end
 
     def self.valid_moves(pos)
@@ -19,7 +20,16 @@ class KnightPathFinder
         moves
     end
 
+    def new_move_positions(pos)
+        debugger
+        possible_moves = KnightPathFinder.valid_moves(pos)
+        new_moves = possible_moves.select { |move| !@considered_positions.include?(move) }
+        @considered_positions.concat(new_moves)
+        return new_moves
+    end
+
 end
 
 x = KnightPathFinder.new([0,0])
-p KnightPathFinder.valid_moves([0,0])
+p x.new_move_positions([0,0])
+# p KnightPathFinder.valid_moves([0,0])
